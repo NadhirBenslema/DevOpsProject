f9fec43c57a076904b3bb0c9e7fdb28ab9e7fe2b
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-10-16 15:10:53 +0100

    FactureTest Updated

--- src/test/java/tn/esprit/rh/achat/FactureTest.java
+++ src/test/java/tn/esprit/rh/achat/FactureTest.java
@@ -1,15 +1,11 @@
 package tn.esprit.rh.achat;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.rh.achat.controllers.FactureRestController;
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.services.IFactureService;
@@ -18,115 +14,92 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
 
-@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {FactureRestController.class})
+@ExtendWith(SpringExtension.class)
 public class FactureTest {
 
-    private MockMvc mockMvc;
-
-    @Mock
+    @MockBean
     private IFactureService factureService;
 
-    @InjectMocks
+    @Autowired
     private FactureRestController factureRestController;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(factureRestController).build();
+    @Test
+    void testRetrieveAllFactures() {
+        List<Facture> factureList = new ArrayList<>();
+        when(factureService.retrieveAllFactures()).thenReturn(factureList);
+        List<Facture> actualFactures = factureRestController.getFactures();
+        assertSame(factureList, actualFactures);
+        assertTrue(actualFactures.isEmpty());
+        verify(factureService).retrieveAllFactures();
     }
 
     @Test
-    public void testGetFactures() throws Exception {
-        // Prepare test data
-        List<Facture> factures = new ArrayList<>();
-        factures.add(new Facture(/* initialize a Facture instance */));
-
-        when(factureService.retrieveAllFactures()).thenReturn(factures);
-
-        mockMvc.perform(get("/facture/retrieve-all-factures"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
-
-        verify(factureService, times(1)).retrieveAllFactures();
-        verifyNoMoreInteractions(factureService);
-    }
-
-    @Test
-    public void testRetrieveFacture() throws Exception {
+    void testRetrieveFacture() {
         Long factureId = 1L;
-        Facture facture = new Facture(/* initialize a Facture instance */);
+        Facture facture = new Facture(); // Initialize a Facture instance
 
         when(factureService.retrieveFacture(factureId)).thenReturn(facture);
-
-        mockMvc.perform(get("/facture/retrieve-facture/{facture-id}", factureId))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
-
-        verify(factureService, times(1)).retrieveFacture(factureId);
-        verifyNoMoreInteractions(factureService);
+        Facture actualFacture = factureRestController.retrieveFacture(factureId);
+        assertSame(facture, actualFacture);
+        verify(factureService).retrieveFacture(factureId);
     }
 
     @Test
-    public void testCancelFacture() throws Exception {
+    void testAddFacture() {
+        Facture facture = new Facture(); // Initialize a Facture instance
+
+        when(factureService.addFacture(facture)).thenReturn(facture);
+        Facture addedFacture = factureRestController.addFacture(facture);
+        assertSame(facture, addedFacture);
+        verify(factureService).addFacture(facture);
+    }
+
+    @Test
+    void testCancelFacture() {
         Long factureId = 1L;
-
-        mockMvc.perform(put("/facture/cancel-facture/{facture-id}", factureId))
-                .andExpect(status().isOk());
-
+        doNothing().when(factureService).cancelFacture(factureId);
+        factureRestController.cancelFacture(factureId);
         verify(factureService, times(1)).cancelFacture(factureId);
-        verifyNoMoreInteractions(factureService);
     }
 
     @Test
-    public void testGetFactureByFournisseur() throws Exception {
+    void testGetFactureByFournisseur() {
         Long fournisseurId = 1L;
-
-        // Prepare test data
-        List<Facture> factures = new ArrayList<>();
-        factures.add(new Facture(/* initialize a Facture instance */));
-
-        when(factureService.getFacturesByFournisseur(fournisseurId)).thenReturn(factures);
-
-        mockMvc.perform(get("/facture/getFactureByFournisseur/{fournisseur-id}", fournisseurId))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
-
-        verify(factureService, times(1)).getFacturesByFournisseur(fournisseurId);
-        verifyNoMoreInteractions(factureService);
+        List<Facture> factureList = new ArrayList<>();
+        when(factureService.getFacturesByFournisseur(fournisseurId)).thenReturn(factureList);
+        List<Facture> actualFactures = factureRestController.getFactureByFournisseur(fournisseurId);
+        assertSame(factureList, actualFactures);
+        assertTrue(actualFactures.isEmpty());
+        verify(factureService).getFacturesByFournisseur(fournisseurId);
     }
 
     @Test
-    public void testAssignOperateurToFacture() throws Exception {
+    void testAssignOperateurToFacture() {
         Long idOperateur = 1L;
         Long idFacture = 2L;
-
-        mockMvc.perform(put("/facture/assignOperateurToFacture/{idOperateur}/{idFacture}", idOperateur, idFacture))
-                .andExpect(status().isOk());
-
+        doNothing().when(factureService).assignOperateurToFacture(idOperateur, idFacture);
+        factureRestController.assignOperateurToFacture(idOperateur, idFacture);
         verify(factureService, times(1)).assignOperateurToFacture(idOperateur, idFacture);
-        verifyNoMoreInteractions(factureService);
     }
 
-   /* @Test
-    public void testPourcentageRecouvrement() throws Exception {
+    @Test
+    void testPourcentageRecouvrement() {
         Date startDate = new Date(); // Set a valid start date
         Date endDate = new Date();   // Set a valid end date
 
         when(factureService.pourcentageRecouvrement(startDate, endDate)).thenReturn(0.75f);
 
-        mockMvc.perform(get("/facture/pourcentageRecouvrement/{startDate}/{endDate}", startDate, endDate))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(content().string("0.75"));
-
-        verify(factureService, times(1)).pourcentageRecouvrement(startDate, endDate);
-        verifyNoMoreInteractions(factureService);
+        float pourcentage = factureRestController.pourcentageRecouvrement(startDate, endDate);
+        assertTrue(pourcentage == 0.75f);
+        verify(factureService).pourcentageRecouvrement(startDate, endDate);
     }
-
-    */
 }
