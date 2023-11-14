8372fd9e629544a7f6b3b2298370b70c84f1595e
ameni <ameni.hajri@esprit.tn>
2023-11-14 23:53:21 +0100

    up prths

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableScheduling
 @SpringBootApplication
-
+@RestController
 public class AchatApplication {
 
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Working...!!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
