930597a17879723603fab670f32c803a01640ac6
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-11-16 02:23:42 +0100

    fix 2nd entity

--- src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des factures")
 @RequestMapping("/facture")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200")
 public class FactureRestController {
 
     @Autowired
