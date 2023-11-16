cd6e83fd2726a7f9f942c07c4aa9f5fa9096de95
AymenMzoughi <48047342+AymenMzoughi@users.noreply.github.com>
2023-11-17 00:01:05 +0100

    Front Angular

---
+++ src/app/add-facture/add-facture.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-facture',
+  templateUrl: './add-facture.component.html',
+  styleUrls: ['./add-facture.component.css']
+})
+export class AddFactureComponent implements OnInit {
+
+  facture: any = {
+    montantFacture: '',
+    montantRemise: '',
+    dateCreationFacture: '',
+   
+  };
+  fournisseurs: any[] = [];
+  apiUrl = 'http://localhost:8089/SpringMVC/';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  navigateToListFacture(): void {
+    this.router.navigate(['/listFacture']);
+  }
+
+
+addFacture(): void {
+  console.log('Facture to be added:', this.facture); // Log before making the HTTP request
+
+  this.http.post<any>(`${this.apiUrl}/facture/add-facture`, JSON.stringify(this.facture), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .subscribe(
+      (facture) => {
+        console.log('Facture added successfully', facture);
+        this.router.navigate(['/listFacture']);
+      },
+      (error) => {
+        console.error('Error adding facture', error);
+      }
+    );
+}
+
+
+}
